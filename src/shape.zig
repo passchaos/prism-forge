d2f0820c7d8598317d7b47378c8351525407977d
passchaos <greedwolf.dss@gmail.com>
2026-01-16 01:51:14 +0800

    feat: support DimExpr broadcast logic

--- src/shape.zig
+++ src/shape.zig
@@ -62,6 +62,36 @@ pub const DimExpr = union(enum) {
     Max: BinaryDimOpExpr,
     Min: BinaryDimOpExpr,
 
+    // may use egraph to optimize
+    pub fn equal(a: Self, b: Self) bool {
+        return switch (a) {
+            .Static => |a_v| switch (b) {
+                .Static => |b_v| a_v == b_v,
+                else => |_| false,
+            },
+            .Sym => |a_s| switch (b) {
+                .Sym => |b_s| a_s.id == b_s.id,
+                else => |_| false,
+            },
+            .Add => |a_v| switch (b) {
+                .Add => |b_v| a_v.lhs.*.equal(b_v.lhs.*) and a_v.rhs.*.equal(b_v.rhs.*),
+                else => |_| false,
+            },
+            .Mul => |a_v| switch (b) {
+                .Mul => |b_v| a_v.lhs.*.equal(b_v.lhs.*) and a_v.rhs.*.equal(b_v.rhs.*),
+                else => |_| false,
+            },
+            .Max => |a_v| switch (b) {
+                .Max => |b_v| a_v.lhs.*.equal(b_v.lhs.*) and a_v.rhs.*.equal(b_v.rhs.*),
+                else => |_| false,
+            },
+            .Min => |a_v| switch (b) {
+                .Min => |b_v| a_v.lhs.*.equal(b_v.lhs.*) and a_v.rhs.*.equal(b_v.rhs.*),
+                else => |_| false,
+            },
+        };
+    }
+
     pub fn static(v: usize) Self {
         return Self{ .Static = v };
     }
@@ -178,6 +208,101 @@ pub inline fn parseSpec(comptime dims: anytype) [utils.stt.getFieldsLenComptime(
     return parsed_dims;
 }
 
+pub fn canBroadcast(
+    comptime lhs: []const DimExpr,
+    comptime rhs: []const DimExpr,
+) bool {
+    const max_len = @max(lhs.len, rhs.len);
+
+    inline for (0..max_len) |i| {
+        const ldim = if (i < lhs.len) lhs[lhs.len - 1 - i] else DimExpr.static(1);
+        const rdim = if (i < rhs.len) rhs[rhs.len - 1 - i] else DimExpr.static(1);
+
+        if (!canBroadcastDim(ldim, rdim)) return false;
+    }
+    return true;
+}
+
+fn canBroadcastDim(comptime a: DimExpr, comptime b: DimExpr) bool {
+    return switch (a) {
+        .Static => |a_v| switch (b) {
+            .Static => |b_v| (a_v == b_v) or (a_v == 1),
+            .Sym => |_| a_v == 1,
+            else => false,
+        },
+        else => a.equal(b),
+    };
+}
+
+pub fn generateBroadcastStride(
+    comptime orig_shape_expr: []const DimExpr,
+    orig_stride: [orig_shape_expr.len]usize,
+    comptime target_shape_expr: []const DimExpr,
+) [target_shape_expr.len]usize {
+    const N = orig_shape_expr.len;
+    const BN = target_shape_expr.len;
+    if (N > BN) @compileError("can't broadcast to smaller dimension");
+
+    var new_stride = [_]usize{0} ** BN;
+
+    inline for (0..BN) |t_idx| {
+        if (N >= t_idx + 1) {
+            const o_dim = orig_shape_expr[N - 1 - t_idx];
+            const t_dim = target_shape_expr[BN - 1 - t_idx];
+
+            // @compileLog(std.fmt.comptimePrint("t_idx: {} o_dim: {} t_dim: {}\n", .{ t_idx, o_dim, t_dim }));
+            switch (o_dim) {
+                .Static => |o_v| {
+                    if (o_v == 1) {
+                        new_stride[BN - 1 - t_idx] = 0;
+                    } else {
+                        if (comptime o_dim.equal(t_dim)) {
+                            new_stride[BN - 1 - t_idx] = orig_stride[N - 1 - t_idx];
+                        } else {
+                            @compileError("can't broadcast " ++ std.fmt.comptimePrint("{} != {}", .{ o_dim, t_dim }));
+                        }
+                    }
+                },
+                else => {
+                    if (comptime o_dim.equal(t_dim)) {
+                        new_stride[BN - 1 - t_idx] = orig_stride[N - 1 - t_idx];
+                    } else {
+                        @compileError("can't broadcast " ++ std.fmt.comptimePrint("{} != {}", .{ o_dim, t_dim }));
+                    }
+                },
+            }
+        } else {
+            new_stride[BN - 1 - t_idx] = 0;
+        }
+    }
+
+    return new_stride;
+}
+
+pub fn compatibleBroacastShapes(comptime lhs_shape: []const usize, comptime rhs_shape: []const usize) [@max(lhs_shape.len, rhs_shape.len)]usize {
+    const l_l = lhs_shape.len;
+    const r_l = rhs_shape.len;
+
+    if (l_l == 0 or r_l == 0) @compileError("can't use zero-d tensor to broadcast");
+
+    const result_len = @max(l_l, r_l);
+
+    comptime var result = [_]usize{0} ** result_len;
+
+    comptime {
+        for (0..result_len) |i| {
+            const v = if (l_l > i) blk: {
+                const v = if (r_l > i) @max(lhs_shape[l_l - i - 1], rhs_shape[r_l - i - 1]) else lhs_shape[l_l - i - 1];
+                break :blk v;
+            } else rhs_shape[r_l - i - 1];
+
+            result[result_len - i - 1] = v;
+        }
+    }
+
+    return result;
+}
+
 test "anonymous_struct" {
     {
         const a1 = "test";
@@ -230,6 +355,37 @@ test "dim_spec" {
     }
 }
 
-test "broadcast_comp" {}
+test "broadcast_comp" {
+    const lhs = comptime DimExpr.static(1);
+
+    const lhs_1 = comptime DimExpr.static(2);
+    const rhs = comptime DimExpr.static(3);
+
+    try std.testing.expect(canBroadcastDim(lhs, rhs));
+    try std.testing.expect(!canBroadcastDim(lhs_1, rhs));
+
+    {
+        const exprs_1 = comptime parseSpec(.{ 2, 3, 4, .{ .name = "ddd" } });
+        const exprs_2 = comptime parseSpec(.{ 2, 3, 4, .{ .name = "ddd" } });
+        try std.testing.expect(!canBroadcast(&exprs_1, &exprs_2));
+    }
+
+    {
+        const exprs_1 = comptime parseSpec(.{ 2, 3, 4, "ddd" });
+        const exprs_2 = comptime parseSpec(.{ 2, 3, 4, "ddd" });
+        std.debug.print("exprs_1: {any}\n", .{exprs_1});
+        std.debug.print("exprs_2: {any}\n", .{exprs_2});
+        try std.testing.expect(canBroadcast(&exprs_1, &exprs_2));
+    }
+
+    {
+        const exprs_1 = comptime parseSpec(.{ 2, 3, 4, "ddd" });
+        const stride_1 = [4]usize{ 1, 2, 3, 4 };
+        const exprs_2 = comptime parseSpec(.{ 1, 3, 1, "ddd" });
+
+        const stride_2 = generateBroadcastStride(&exprs_2, stride_1, &exprs_1);
+        std.debug.print("stride_2: {any}\n", .{stride_2});
+    }
+}
 
 test "shape creation" {}
